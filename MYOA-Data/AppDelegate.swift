6c9e811b2a8f5a161ceb9362b808dfe1c6b37ee0
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-11 16:08:24 -0700

    Updated Files
    Updated files.

---
+++ MYOA-Data/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MYOA-Data
+//
+//  Created by Apple Computer on 6/11/23.
+//
+
+import UIKit
+
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Properties
+    
+    var window: UIWindow?
+    
+    // MARK: UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
